22484e8eded7e5733621042a4a90ae9a93b21496
PrzemekGdula <przemo.g12@wp.pl>
2019-04-19 14:25:20 +0200

    simple input added start

---
+++ src/SimpleInput.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const SimpleInput = (props) => (
+    <div>
+        <h4>
+
+        </h4>
+<input
+value={''}
+onChange={()=>{}}
+/>
+
+    </div>
+)
+
+export default SimpleInput
